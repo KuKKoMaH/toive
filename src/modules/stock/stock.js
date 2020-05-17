cc778de7d981109ab746457ced6bffcdb83e241f
KuKKoMaH <kukkoman@gmail.com>
2020-05-17 23:18:55 +0300

    страница акции

--- src/modules/stock/stock.js
+++ src/modules/stock/stock.js
@@ -1,6 +1,6 @@
 import initSlider from '../../js/initSlider';
 
-$('.stock__more').on('click', () => $('.stock').addClass('stock--active'))
+$('.stock__more').on('click', () => $('.stock').addClass('stock--active'));
 
 initSlider('.stock__slider', {
   slidesPerView: 1,
@@ -13,3 +13,9 @@ initSlider('.stock__slider', {
     nextEl: $('.stock__pagination .arrow--right')[0],
   },
 }, { sm: true, md: true, lg: false });
+
+$('.stock__secondary').on('click', ( e ) => {
+  const $el = $(e.delegateTarget);
+  const $parent = $el.parents('.stock__item');
+  $parent.find('.stock__primary').css('background-image', `url(${$el.data('image')})`)
+});
